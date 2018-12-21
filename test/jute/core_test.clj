d5a3870b34d889da86ba083aa94d776f436355f2
niquola <niquola@gmail.com>
2018-12-21 11:51:57 +0300

    add fhirpath

--- test/jute/core_test.clj
+++ test/jute/core_test.clj
@@ -1,5 +1,6 @@
 (ns jute.core-test
   (:require [clojure.test :refer :all]
+            [fhirpath.core :as fp]
             [jute.core :refer :all]))
 
 (deftest expressions-test
@@ -12,6 +13,7 @@
   (is (= ((compile* "$ foo + 42") {:foo 5}) 47))
   ;;(is (= ((compile* "$ foo + 42") {:foo "a"}) "a42"))
 
+  (is (= 1 (fp/fp "a.b.c" {:a {:b {:c 1}}})))
   )
 
 (run-tests)
